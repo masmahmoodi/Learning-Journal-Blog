38e2c7c4d5da76527e96a89b03a93e74c8eb4a89
masmahmoodi <mrm7g8@gmail.com>
2024-02-29 22:09:16 +0430

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// displaying header
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+
+
+
+// displaying footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    })
+
+const viewMoreBlogs = document.querySelector(".more")
+const displaying = document.querySelector(".show-more")
+
+viewMoreBlogs.addEventListener("click", ()=>{
+    displaying.classList.toggle("hidden")
+})
+
+
+
+
+
